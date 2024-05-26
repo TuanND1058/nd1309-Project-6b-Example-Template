6b331162377ce36d120b3a9bac60f5213cfa08be
TuanND <tuannd1058@gmail.com>
2024-05-26 21:10:12 +0700

    setup: update deploy

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,5 +1,5 @@
 const HDWallet = require('truffle-hdwallet-provider');
-const infuraKey = "62f170c81c504b21965a46a355e69347";
+const infuraKey = "b8e77f70fe4d4ef5960285c0864618c5";
 
 const fs = require('fs');
 const mnemonic = fs.readFileSync("../.secret").toString().trim();
@@ -15,7 +15,9 @@ module.exports = {
       provider: () => new HDWallet(mnemonic, `https://sepolia.infura.io/v3/${infuraKey}`),
       network_id: 11155111,
       gas: 4500000,
-      gasPrice: 10000000000
+      gasPrice: 10000000000,
+      networkCheckTimeout: 100000, // Increase the timeout to 100 seconds
+      timeoutBlocks: 200, // Increase the number of blocks to wait before timing out
     },
-  }
+  },
 };
