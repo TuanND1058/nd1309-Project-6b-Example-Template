ca53cf4093b5bce3172c57366b4df7f8fa299404
TuanND <tuannd1058@gmail.com>
2024-05-26 19:03:31 +0700

    coding: add testcase 6

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -176,19 +176,23 @@ contract('SupplyChain', function(accounts) {
         const supplyChain = await SupplyChain.deployed()
 
         // Declare and Initialize a variable for event
-
+        var eventEmitted = false
 
         // Watch the emitted event Shipped()
-
+        supplyChain.Shipped((err, res) => {
+            eventEmitted = true
+        })
 
         // Mark an item as Sold by calling function shipItem()
-
+        await supplyChain.shipItem(upc, {from: distributorID})
 
         // Retrieve the just now saved item from blockchain by calling function fetchItem()
-
+        const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc)
+        const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc)
 
         // Verify the result set
-
+        assert.equal(resultBufferTwo[5], 5, 'Error: Invalid item State')
+        assert.equal(eventEmitted, true, 'Invalid event emitted')
     })
 
     // 7th Test
